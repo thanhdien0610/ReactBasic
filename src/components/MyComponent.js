53605550d1651170a500b5a867fba5582b4ec651
sylver0610 <79132069+sylver0610@users.noreply.github.com>
2022-10-17 21:24:32 +0300

    use form and onChange, onSubmit

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -23,12 +23,26 @@ class MyComponent extends React.Component {
         console.log(event.target);
     }
 
+    handleOnChangeInput = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
+    handleOnSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state);
+    }
+
     //JSX
     render() {
         return (
             <div> my first component {this.state.name} and {this.state.age}
-                <button onMouseOver={this.handleOnMouseOver}> Hover me</button>
-                <button onClick={(event) => { this.handleClick(event) }}> Click me</button>
+
+                <form onSubmit={(event) => { this.handleOnSubmit(event) }}>
+                    <input type="text" onChange={(event) => { this.handleOnChangeInput(event) }} />
+                    <button> Submit</button>
+                </form>
             </div>
         );
     }
