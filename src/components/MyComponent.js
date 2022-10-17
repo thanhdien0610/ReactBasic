8ee838c49c9c927b4532904eaafb89b1d95d5a5f
sylver0610 <79132069+sylver0610@users.noreply.github.com>
2022-10-17 21:10:35 +0300

    use state

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,11 +1,34 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+
+    state = {
+        name: 'Dien',
+        address: 'Moscow',
+        age: 22
+    };
+
+    handleClick = (event) => {
+
+        console.log("Click me");
+
+        this.setState({
+            name: 'Ladien',
+            age: Math.floor((Math.random() * 100) + 1)
+        })
+        console.log("My name is ", this.state.name);
+    }
+
+    handleOnMouseOver(event) {
+        console.log(event.target);
+    }
+
     //JSX
     render() {
         return (
-            <div> my first component
-                { }
+            <div> my first component {this.state.name} and {this.state.age}
+                <button onMouseOver={this.handleOnMouseOver}> Hover me</button>
+                <button onClick={(event) => { this.handleClick(event) }}> Click me</button>
             </div>
         );
     }
