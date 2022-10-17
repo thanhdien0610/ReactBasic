6be8973737bf2a2fd1bf2869030be676a3dad1d8
sylver0610 <79132069+sylver0610@users.noreply.github.com>
2022-10-17 23:41:39 +0300

    Hide/ Show list user and change color infoUser

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,26 +1,43 @@
 import React from "react";
 
 class DisplayInfo extends React.Component {
+
+    state = {
+        isShowList: true
+    }
+
+    handleShowHide(event) {
+        this.setState({
+            isShowList: !this.state.isShowList
+        })
+    }
+
     render() {
         const { listUsers } = this.props;
-
+        console.log(listUsers)
         return (
-
             <div>
-                {listUsers.map((user) => {
-                    return (
-                        <div key={user.id}>
-                            <div> My name is {user.name} </div>
-                            <div> My age is {user.age} </div>
-                            <hr />
-                        </div>
-                    )
-                })}
-
+                <div>
+                    <span onClick={(event) => { this.handleShowHide(event) }}>
+                        {this.state.isShowList ? "Hide " : "Show "}  list users:</span>
+                </div>
+                {this.state.isShowList &&
+                    <div>
+                        {listUsers.map((user) => {
+                            return (
+                                <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+                                    <div> My name is {user.name} </div>
+                                    <div> My age is {user.age} </div>
+                                    <hr />
+                                </div>
+                            )
+                        })
+                        }
+                    </div>}
             </div>
-
         )
     }
 }
+
 
 export default DisplayInfo;
