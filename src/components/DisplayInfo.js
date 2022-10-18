aae675fe7832987e36e3c009fb303b4bab244675
sylver0610 <79132069+sylver0610@users.noreply.github.com>
2022-10-18 21:10:33 +0300

    use stateless and stateful

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -3,97 +3,74 @@ import './DisplayInfo.scss'
 import logo from './../logo.svg'
 
 import AddUserInfo from "./AddUserInfo";
-class DisplayInfo extends React.Component {
 
-    constructor(props) {
-        console.log("did mount 0");
-        super(props);
-        this.state = {
-            isShowList: true
-        }
+//stateful
+// class DisplayInfo extends React.Component {
 
-    }
+//     render() {
+//         console.log("render ");
+//         const { listUsers } = this.props;
+//         console.log(listUsers)
+//         return (
+//             <div className="display-info-container" key={Math.floor((Math.random() * 100) + 1)}>
 
-    componentDidMount() {
-        console.log("did mount");
-        setTimeout(() => {
-            document.title = 'LaDien'
-        }, 3000)
-    }
+//                 {true &&
+//                     <>
+//                         {listUsers.map((user) => {
+//                             return (
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        console.log('did update', this.props, prevProps)
-        if (this.props.listUsers !== prevProps.listUsers) {
-            if (this.props.listUsers.length === 5) {
-                alert('You got 5 users')
-            }
-        }
-    }
+//                                 <div key={user.id}>
+//                                     <AddUserInfo key={user.id}
+//                                         state={user}
+//                                         handleDeleteUser={this.props.handleDeleteUser}
+//                                     />
 
+//                                     <div key="{user.id}-btn">
+//                                         <button onClick={() => this.props.handleDeleteUser(user.id)}>Delete</button>
+//                                     </div>
 
-    handleShowHide(event) {
-        this.setState({
-            isShowList: !this.state.isShowList
-        })
-    }
+//                                     <hr key="{user.id}-br" />
+//                                 </div>
+//                             )
+//                         })
+//                         }
+//                     </>}
+//             </div>
+//         )
+//     }
+// }
 
-    handleSubmit(event) {
-        event.preventDefault();
-        alert('updated');
-    }
+//stateless
+const DisplayInfo = (props) => {
+    const { listUsers } = props;
 
+    console.log(listUsers)
+    return (
+        <div className="display-info-container" key={Math.floor((Math.random() * 100) + 1)}>
 
+            {true &&
+                <>
+                    {listUsers.map((user) => {
+                        return (
 
+                            <div key={user.id}>
+                                <AddUserInfo key={user.id}
+                                    state={user}
+                                    handleDeleteUser={props.handleDeleteUser}
+                                />
 
-    render() {
-        console.log("render ");
-        const { listUsers } = this.props;
-        console.log(listUsers)
-        return (
-            <div className="display-info-container" key={Math.floor((Math.random() * 100) + 1)}>
-                {/* <img src={logo} /> */}
-
-                <span onClick={(event) => { this.handleShowHide(event) }}>
-                    {this.state.isShowList ? "Hide " : "Show "}  list users:</span>
-
-                {this.state.isShowList &&
-                    <>
-                        {listUsers.map((user) => {
-                            return (
-                                // <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-                                //     <div>
-                                //         <div> My name is {user.name} </div>
-                                //         <div> My age is {user.age} </div>
-                                //     </div>
-                                //     <div>
-                                //         <button onClick={() => this.props.handleDeleteUser(user.id)}>Delete</button>
-                                //         {/* <UpdateUser
-                                //             userInfo={user}
-                                //         /> */}
-                                //     </div>
-                                //     <hr />
-                                // </div>
-                                <div key={user.id}>
-                                    <AddUserInfo key={user.id}
-                                        state={user}
-                                        handleDeleteUser={this.props.handleDeleteUser}
-                                    />
-
-                                    <div key="{user.id}-btn">
-                                        <button onClick={() => this.props.handleDeleteUser(user.id)}>Delete</button>
-                                        {/* <UpdateUser
-                                             userInfo={user}
-                                         /> */}
-                                    </div>
-                                    <hr key="{user.id}-br" />
+                                <div key="{user.id}-btn">
+                                    <button onClick={() => props.handleDeleteUser(user.id)}>Delete</button>
                                 </div>
-                            )
-                        })
-                        }
-                    </>}
-            </div>
-        )
-    }
+
+                                <hr key="{user.id}-br" />
+                            </div>
+                        )
+                    })
+                    }
+                </>}
+        </div>
+    )
 }
 
 
