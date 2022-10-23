70bc82ad2a9707d62261326c190b45bcf8f60b13
sylver0610 <79132069+sylver0610@users.noreply.github.com>
2022-10-23 20:58:44 +0300

    update sidebar

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -2,15 +2,19 @@ import SideBar from "./SideBar";
 import './Admin.scss'
 import { useState } from "react";
 import { FaHeart, FaBars } from 'react-icons/fa';
+import { ProSidebarProvider } from "react-pro-sidebar";
 const Admin = (props) => {
-    const [collapsed, setCollapsed] = useState(false);
+    // const [collapsed, setCollapsed] = useState(false);
     return (
         <div className="admin-container">
-            <div className="admin-sidebar">
+            <ProSidebarProvider className="admin-sidebar">
+                <SideBar />
+            </ProSidebarProvider>;
+            {/* <div className="admin-sidebar">
                 <SideBar collapsed={collapsed} />
-            </div>
+            </div> */}
             <div className="admin-content">
-                <FaBars onClick={() => setCollapsed(!collapsed)} />
+                {/* <FaBars onClick={() => setCollapsed(!collapsed)} /> */}
                 contain
             </div>
         </div>
--- src/components/Admin/SideBar.js
+++ src/components/Admin/SideBar.js
@@ -1,92 +1,37 @@
 import React from 'react';
-import 'react-pro-sidebar/dist/css/styles.css'
-import {
-    ProSidebar,
-    Menu,
-    MenuItem,
-    SubMenu,
-    SidebarHeader,
-    SidebarFooter,
-    SidebarContent,
-} from 'react-pro-sidebar';
-import { FaTachometerAlt, FaGem, FaList, FaGithub, FaRegLaughWink, FaHeart } from 'react-icons/fa';
-import sidebarBg from '../../assets/bg2.jpg';
-//import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
-//import { FaHeart, FaGem } from 'react-icons/fa'
+//import 'react-pro-sidebar/dist/css/styles.css'
+import { Sidebar, Menu, MenuItem, useProSidebar, SubMenu } from 'react-pro-sidebar';
+import { FaGem, FaList, FaBars } from 'react-icons/fa';
+import { MdDashboard } from 'react-icons/md'
+import './Admin.scss'
 const SideBar = (props) => {
-    const { image, collapsed, toggled, handleToggleSidebar } = props;
+    const { collapseSidebar } = useProSidebar();
     return (
         <>
-            <ProSidebar
-                image={sidebarBg}
 
-                collapsed={collapsed}
-                toggled={toggled}
-                breakPoint="md"
-                onToggle={handleToggleSidebar}
-            >
-                <SidebarHeader>
-                    <div
-                        style={{
-                            padding: '24px',
-                            textTransform: 'uppercase',
-                            fontWeight: 'bold',
-                            fontSize: 14,
-                            letterSpacing: '1px',
-                            overflow: 'hidden',
-                            textOverflow: 'ellipsis',
-                            whiteSpace: 'nowrap',
-                        }}
-                    >
-                        MySideBar
-                    </div>
-                </SidebarHeader>
+            <Sidebar>
+                <Menu>
+                    <MenuItem icon={<MdDashboard />}> Dashboard</MenuItem>
+                    {/* <MenuItem icon={<FaList />}> Calendar</MenuItem>
+                    <MenuItem icon={<FaList />}> E-commerce</MenuItem> */}
+                </Menu>
+                <Menu>
+                    <SubMenu className='subMenu'
+                        // suffix={<span className="badge yellow">3</span>}
+                        label="Features" icon={<FaGem />}>
+                        <MenuItem> Quản lý Users </MenuItem>
+                        <MenuItem> Quản lý Quiz </MenuItem>
+                        <MenuItem> Quản lý câu hỏi </MenuItem>
+                    </SubMenu>
+                    {/* <MenuItem>Hahaha</MenuItem> */}
+                </Menu>
 
-                <SidebarContent>
-                    <Menu iconShape="circle">
-                        <MenuItem
-                            icon={<FaTachometerAlt />}
-                            suffix={<span className="badge red">New</span>}
-                        >
-                            dashboard
-                        </MenuItem>
-                        <MenuItem icon={<FaGem />}> components</MenuItem>
-                    </Menu>
-                    <Menu iconShape="circle">
-                        <SubMenu
-                            suffix={<span className="badge yellow">3</span>}
 
-                            icon={<FaRegLaughWink />}
-                        >
-                            <MenuItem> 1</MenuItem>
-                            <MenuItem> 2</MenuItem>
-                            <MenuItem> 3</MenuItem>
-                        </SubMenu>
+            </Sidebar>
+            <Menu className='sidebar'>
+                <MenuItem icon={<FaBars />} onClick={() => collapseSidebar()}>  </MenuItem>
+            </Menu>
 
-                    </Menu>
-                </SidebarContent>
-
-                <SidebarFooter style={{ textAlign: 'center' }}>
-                    <div
-                        className="sidebar-btn-wrapper"
-                        style={{
-                            padding: '20px 24px',
-                        }}
-                    >
-                        <a
-                            href="https://github.com/azouaoui-med/react-pro-sidebar"
-                            target="_blank"
-                            className="sidebar-btn"
-                            rel="noopener noreferrer"
-                        >
-                            <FaGithub />
-                            <span style={{ whiteSpace: 'nowrap', textOverflow: 'ellipsis', overflow: 'hidden' }}>
-                                viewSource
-                            </span>
-                        </a>
-                    </div>
-                </SidebarFooter>
-            </ProSidebar>
         </>
 
 
