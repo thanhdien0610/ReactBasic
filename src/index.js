36db2f8489f8541f74d929176fbff89c5c5263db
sylver0610 <79132069+sylver0610@users.noreply.github.com>
2023-03-04 02:10:19 +0300

    use NProgress to create loading bar

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import { store } from './redux/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from './Layout';
-
+// import '../styles/globals.css'
+// import '../styles/nprogress.css'
+import 'nprogress/nprogress.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
